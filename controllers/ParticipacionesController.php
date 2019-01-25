21ab1c1ebe3672c3756fe32b0d8c0e8bf67df90c
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2019-01-25 21:04:14 +0100

    Puesta a punto

--- controllers/ParticipacionesController.php
+++ controllers/ParticipacionesController.php
@@ -2,119 +2,36 @@
 
 namespace app\controllers;
 
-use app\models\BuscarForm;
-use app\models\Generos;
+use app\models\Participaciones;
 use app\models\Peliculas;
-use Yii;
-use yii\data\Sort;
-use yii\web\NotFoundHttpException;
 
-/**
- * Definición del controlador peliculas.
- */
-class PeliculasController extends \yii\web\Controller
+class ParticipacionesController extends \yii\web\Controller
 {
-    public function actionPrueba()
+    public function actionUpdate($pelicula_id)
     {
-        Yii::$app->session->setFlash('error', 'Esto es un error.');
-        return $this->redirect(['peliculas/index']);
-    }
-
-    public function actionIndex()
-    {
-        $sort = new Sort([
-            'attributes' => [
-                'titulo',
-                'anyo',
-                'duracion',
-                'genero',
-            ],
-        ]);
-
-        $buscarForm = new BuscarForm();
-        $query = Peliculas::find()->with('genero');
-
-        if ($buscarForm->load(Yii::$app->request->post()) && $buscarForm->validate()) {
-            $query->andFilterWhere(['ilike', 'titulo', $buscarForm->titulo]);
-            $query->andFilterWhere(['genero_id' => $buscarForm->genero_id]);
-        }
-
-        if (empty($sort->orders)) {
-            $query->orderBy(['id' => SORT_ASC]);
-        } else {
-            $query->orderBy($sort->orders);
-        }
-
-        return $this->render('index', [
-            'peliculas' => $query->all(),
-            'sort' => $sort,
-            'listaGeneros' => ['' => ''] + $this->listaGeneros(),
-            'buscarForm' => $buscarForm,
-        ]);
-    }
-
-    public function actionCreate()
-    {
-        $pelicula = new Peliculas();
-
-        if ($pelicula->load(Yii::$app->request->post()) && $pelicula->save()) {
-            return $this->redirect(['peliculas/index']);
-        }
-        return $this->render('create', [
-            'pelicula' => $pelicula,
-        ]);
-    }
-
-    public function actionVer($id)
-    {
-        $pelicula = $this->buscarPelicula($id);
-        $pelicula->genero_id = $pelicula->genero->genero;
-
-        return $this->render('ver', [
-            'pelicula' => $pelicula,
-        ]);
-    }
-
-    public function actionUpdate($id)
-    {
-        $pelicula = $this->buscarPelicula($id);
-
-        if ($pelicula->load(Yii::$app->request->post()) && $pelicula->save()) {
-            return $this->redirect(['peliculas/index']);
-        }
+        $pelicula = Peliculas::findOne($pelicula_id);
+        $participaciones = Participaciones::find()
+            ->where(['pelicula_id' => $pelicula_id])
+            ->all();
 
         return $this->render('update', [
             'pelicula' => $pelicula,
-            'listaGeneros' => $this->listaGeneros(),
+            'participaciones' => $participaciones,
         ]);
     }
 
-    public function actionDelete($id)
+    public function actionDelete($pelicula_id, $persona_id, $papel_id)
     {
-        $this->buscarPelicula($id)->delete();
-        return $this->redirect(['peliculas/index']);
-    }
+        $participacion = Participaciones::findOne([
+            'pelicula_id' => $pelicula_id,
+            'persona_id' => $persona_id,
+            'papel_id' => $papel_id,
+        ]);
 
-    private function listaGeneros()
-    {
-        return Generos::find()
-            ->select('genero')
-            ->indexBy('id')
-            ->column();
-    }
-
-    private function buscarPelicula($id)
-    {
-        $fila = Peliculas::find()
-            ->where(['id' => $id])
-            ->with([
-                'participaciones.persona',
-                'participaciones.papel',
-            ])
-            ->one();
-        if ($fila === null) {
-            throw new NotFoundHttpException('Esa película no existe.');
-        }
-        return $fila;
+        $participacion->delete();
+        return $this->redirect([
+            'participaciones/update',
+            'pelicula_id' => $pelicula_id,
+        ]);
     }
 }
