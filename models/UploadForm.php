92a12b3963fa61a1543a123945b8991ef5b02ca3
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2019-02-07 18:25:22 +0100

    Puesta al día 07/02

---
+++ models/UploadForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class UploadForm extends Model
+{
+    /**
+     * @var UploadedFile
+     */
+    public $imageFile;
+
+    public function rules()
+    {
+        return [
+            [['imageFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg'],
+        ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+            $this->imageFile->saveAs(Yii::getAlias('@uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension));
+            return true;
+        }
+        return false;
+    }
+}
